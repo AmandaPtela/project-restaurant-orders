22489973662030a686189e5df2ec280ad1b334f1
Amanda Portela <mandaptela@gmail.com>
2023-08-16 21:08:22 -0300

    base testes dish

--- tests/dish/test_dish.py
+++ tests/dish/test_dish.py
@@ -3,4 +3,27 @@ from src.models.dish import Dish  # noqa: F401, E261, E501
 
 # Req 2
 def test_dish():
-    pass
+    # 2.1 - o atributo name de um prato diferente que o passado ao construtor.
+    # PAREI AQUI ---- CONTINUAR AQUI
+    # 2.2 - caso os hashes de dois pratos iguais sejam diferentes;
+
+    # 2.3 - caso os hashes de dois pratos diferentes sejam iguais;
+
+    # 2.4 - comparação de igualdade de dois pratos iguais (ou de um prato com ele mesmo) seja falsa;
+
+    # 2.5 - caso a comparação de igualdade de dois pratos diferentes seja verdadeira;
+
+    # 2.6 - implementação do método __repr__ retorne um valor inadequado;
+
+    # 2.7 - caso um TypeError não seja levantado ao criar um prato com um valor de tipo inválido;
+
+    # 2.8 - caso um ValueError não seja levantado ao criar um prato com um valor inválido;
+
+    # 2.9 - caso o acesso a um valor do atributo recipe, ao ser indexado com um ingrediente válido retorne uma quantidade inválida (dica: use o método get do dicionário, por exemplo dish.recipe.get(ingredient));
+
+    # 2.10 - caso o método get_restrictions retorne um set de restrições diferente do esperado;
+
+    # 2.11 - caso o método get_ingredients retorne um set de ingredientes diferente do esperado;
+
+    # 2.12 - PASSA com a implementação correta da classe Dish
+    print('ok')
